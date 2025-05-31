bd263c6296f31540bf7b32a1b7f0120f9e81a203
Zack Tzeng <zack.gy.tzeng@gmail.com>
2025-05-31 21:18:00 +0200

    Implement send transaction

---
+++ lib/abi.ts
@@ -0,0 +1,27 @@
+export const ABI = [{
+    inputs: [
+        {
+            internalType: "uint256",
+            name: "nullifierHash",
+            type: "uint256",
+        },
+        {
+            internalType: "bytes",
+            name: "signature",
+            type: "bytes",
+        },
+    ],
+    name: "mint",
+    outputs: [
+        {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+        },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+}];
+
+export const CONTRACT_ADDRESS =
+    "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
